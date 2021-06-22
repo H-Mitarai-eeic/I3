af2cd86ae14978c1f5f00e46999340e1e6f0fd10
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-06-22 15:34:06 +0900

    identify_f0.c とりあえず完成

--- I3.m
+++ I3.m
@@ -2,12 +2,12 @@ filename1 = ("./output/ACF.dat");
 data1 = readmatrix(filename1);
 f1 = figure;
 plot(data1(:,2));
-
-xlim([0, 8192])
+samplenum = 8192;
+xlim([0, samplenum])
 
 filename2 = ('./output/PSD_re.dat');
 data2 = readmatrix(filename2);
 f2 = figure;
 plot(data2(:, 2));
-xlim([0 8182])
+xlim([samplenum samplenum*2])
 %ylim([0 1e10])
--- src/test_print.c
+++ src/test_print.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 
-int main(void){
-    int N = 52;
+#define KeyNum 88
+void print_keyboard(char *key, int N){
+    //int N = 52;
     int i;
     printf("\x1b[30m\x1b[47m|");
     for(i = 0; i < N; i++){
-        if(i % 7 == 0 || i % 7 == 2 || i % 7 == 3 || i % 7 == 5 || i % 7 == 6 ){
-            printf(" \x1b[40m_\x1b[47m");
+        if(i % 12 == 1 || i % 12 == 4 || i % 12 == 6 || i % 12 == 9 || i % 12 == 11){
+            if(key[i] == 1){
+                printf("\x1b[41m \x1b[47m");
+            }
+            else{
+                printf("\x1b[40m \x1b[47m");
+            }
         }
-        else{
+        else if (i % 12 == 2 || i % 12 == 7){
             printf(" |");
         }
+        else{
+            printf(" ");
+        }
     }
-    printf("\n\x1b[47m");
+    printf("\n\x1b[47m|");
     for (i = 0; i < N; i++){
-        printf("|_");
+        if(i % 12 == 1 || i % 12 == 4 || i % 12 == 6 || i % 12 == 9 || i % 12 == 11){
+            printf("|");
+        }
+        else if (i % 12 == 2 || i % 12 == 7){
+            if (key[i] == 1){
+                printf("\x1b[41m_\x1b[47m|");            
+            }
+            else{
+                printf("_|");
+            }
+        }
+        else{
+            if (key[i] == 1){
+                printf("\x1b[41m_\x1b[47m");            
+            }
+            else{
+                printf("_");
+            }
+        }
     }
-    printf("|\x1b[49m\x1b[39m \n");
+    printf("|\x1b[49m\x1b[39m\n");
+    fflush(stdout);
+}
+
+int main(void){
+    char key[KeyNum] = {0};
+    key[0] = 1;
+    key[1] = 1;
+    print_keyboard(key, KeyNum);
+    return 0;
 }
